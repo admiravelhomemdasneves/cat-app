9ae0eb1a6e54564b0aac09a2da7dba7127275c79
admiravelhomemdasneves <andre.santos.loureiro@gmail.com>
2024-10-10 17:50:14 +0100

    Fixing Email Sending Service

--- src/main/java/com/camaja/catapp/services/EmailService.java
+++ src/main/java/com/camaja/catapp/services/EmailService.java
@@ -3,6 +3,7 @@ package com.camaja.catapp.services;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,15 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
         MimeMessage message = emailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        helper.setTo(to);
+        helper.setTo("<" + to + ">");
+        helper.setFrom("<" + from + ">");
         helper.setSubject(subject);
         helper.setText(text, true);
 
